1a781db00078358c573712de767967e0265f660e
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2023-08-01 11:15:25 +0600

    restrutureing

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,95 @@
+import 'package:assignment_1/screens/taskboard.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 282,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    "Welcome to",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.inter(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  Text(
+                    "Plan IT",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.roboto(
+                        fontSize: 32, fontWeight: FontWeight.w900),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              "Your Personal task management and planning solution",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.inter(
+                fontSize: 12,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 18,
+              ),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const TaskBoardPage(),
+                    ),
+                  );
+                },
+                style: const ButtonStyle(
+                  backgroundColor:
+                      MaterialStatePropertyAll<Color>(Colors.black),
+                  foregroundColor:
+                      MaterialStatePropertyAll<Color>(Colors.white),
+                  padding: MaterialStatePropertyAll<EdgeInsets>(
+                      EdgeInsets.symmetric(vertical: 10, horizontal: 50)),
+                  shape: MaterialStatePropertyAll<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                    ),
+                  ),
+                ),
+                child: Text(
+                  "Let's get started",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.inter(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
